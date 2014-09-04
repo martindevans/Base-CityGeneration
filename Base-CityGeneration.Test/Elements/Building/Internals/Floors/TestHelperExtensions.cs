7058bb95a1c4d31e940694cacc99db066eeeeb3b
Martin Evans <martindevans@gmail.com>
2014-09-04 23:38:56 +0100

    Tweaked how BaseFloor detects external walls and matches them up with facades (allow 5cm slack to handle rooms being shrunk slightly in plan generation)

---
+++ Base-CityGeneration.Test/Elements/Building/Internals/Floors/TestHelperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xna.Framework;
+
+namespace Base_CityGeneration.Test.Elements.Building.Internals.Floors
+{
+    public static class TestHelperExtensions
+    {
+        public static bool RoughlyContains(this IEnumerable<Vector2> points, Vector2 point, float epsilon)
+        {
+            return points.Any(a => Vector2.Distance(a, point) <= epsilon);
+        }
+    }
+}
