1c75128fce262898d5588af8a8ce90f67836a04f
Martin <Martin@martin-pc>
2015-08-07 16:25:04 +0100

    Updated packages

--- Base-CityGeneration/Elements/Generic/SolidPlaceholderBlock.cs
+++ Base-CityGeneration/Elements/Generic/SolidPlaceholderBlock.cs
@@ -20,7 +20,6 @@ namespace Base_CityGeneration.Elements.Generic
             _parceller = new ObbParceller(Random);
             _parceller.AddTerminationRule(new AreaRule(100, 300, 0.25f));
             _parceller.AddTerminationRule(new FrontageRule(25, 50, 0.45f, "road"));
-            _parceller.AddTerminationRule(new AccessRule("road", 0.75f));
         }
 
         public override bool Accept(Prism bounds, INamedDataProvider parameters)
