bf20a307803dab885ad959ecdfa26c18bebfdd0f
Martin <Martin@martin-pc>
2015-09-08 15:58:19 +0100

    Added a description to package

--- Base-CityGeneration/Properties/AssemblyInfo.cs
+++ Base-CityGeneration/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Base-CityGeneration")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Procedural City Generation Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Base-CityGeneration")]
