aafaedfdcebb14fcb609f48f0689f1ad4c781130
Martin Evans <martindevans@gmail.com>
2015-09-10 02:09:06 +0100

    Added footprint generation to spec building

--- Base-CityGeneration/Elements/Building/SpecBuildingContainer.cs
+++ Base-CityGeneration/Elements/Building/SpecBuildingContainer.cs
@@ -9,7 +9,7 @@ using Myre.Collections;
 
 namespace Base_CityGeneration.Elements.Building
 {
-    public class SpecBuildingContainer
+    public abstract class SpecBuildingContainer
         : BaseBuildingContainer
     {
         public static readonly TypedName<Design.Internals> BuildingInternalsName = new TypedName<Design.Internals>("building_internals");
@@ -17,7 +17,7 @@ namespace Base_CityGeneration.Elements.Building
         private readonly BuildingDesigner _designer;
         private Design.Internals _internals;
 
-        public SpecBuildingContainer(BuildingDesigner designer)
+        protected SpecBuildingContainer(BuildingDesigner designer)
             : base(designer.MaxHeight, float.MaxValue)  //We ensure that building will fit into the given space by setting *min* allowable space to *max* possible building height
         {
             _designer = designer;
