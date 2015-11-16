aecad6ab2692d9862d2109db6d5bda26c6aedb57
Martin Evans <martindevans@gmail.com>
2015-11-16 16:44:16 +0000

    Updated packages

---
+++ Base-CityGeneration/Entry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+using EpimetheusPlugins.Scripts;
+using Ninject;
+
+namespace Base_CityGeneration
+{
+    public class Entry
+        : IPluginEntryPoint
+    {
+        public void Loaded(IKernel kernel)
+        {
+            Console.WriteLine("Loaded(IKernel) Base-CityGeneration");
+
+            CGAL_StraightSkeleton_Dotnet.StraightSkeleton.Generate(new Vector2[] {
+                new Vector2(10, 10),
+                new Vector2(10, -10),
+                new Vector2(-10, -10),
+                new Vector2(-10, 10),
+            });
+        }
+    }
+}
