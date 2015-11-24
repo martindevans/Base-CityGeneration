1261f6eee774e99130c684ff325781b199deabe1
Martin Evans <martindevans@gmail.com>
2015-11-24 00:48:52 +0000

    Experimented with floorplan generation. Made some headway with slicing floor up using straight skeleton

--- Base-CityGeneration/Elements/Building/Internals/Floors/Design/FloorDesigner.cs
+++ Base-CityGeneration/Elements/Building/Internals/Floors/Design/FloorDesigner.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Numerics;
+using System.Text;
 using Base_CityGeneration.Elements.Building.Internals.Floors.Design.Connections;
 using Base_CityGeneration.Elements.Building.Internals.Floors.Design.Constraints;
 using Base_CityGeneration.Elements.Building.Internals.Floors.Design.Spaces;
 using Base_CityGeneration.Elements.Building.Internals.Floors.Plan;
 using Base_CityGeneration.Utilities.Numbers;
 using CGAL_StraightSkeleton_Dotnet;
+using EpimetheusPlugins.Procedural.Utilities;
+using EpimetheusPlugins.Scripts;
 using JetBrains.Annotations;
+using Myre.Collections;
 using SharpYaml.Serialization;
 using SwizzleMyVectors.Geometry;
+
+using MathHelper = Microsoft.Xna.Framework.MathHelper;
 
 namespace Base_CityGeneration.Elements.Building.Internals.Floors.Design
 {
@@ -38,67 +44,56 @@ namespace Base_CityGeneration.Elements.Building.Internals.Floors.Design
             _rooms = rooms;
         }
 
-        public FloorPlan Design(IReadOnlyList<Vector2> footprint)
+        public FloorPlan Design(Func<double> random, INamedDataCollection metadata, Func<KeyValuePair<string, string>[], Type[], ScriptReference> finder, IReadOnlyList<Vector2> footprint)
         {
-            using (var skeleton = StraightSkeleton.Generate(footprint))
-            {
-                //check if skeleton is too small, or too far from walls - if it is generate an offset skeleton
-                float max;
-                float min;
-                MeasureSkeletonDistance(skeleton, out min, out max);
+            //Generate set of required spaces
+            var requiredSpecs = _rooms.SelectMany(r => r.Produce(true, random, metadata));
 
-                ///////////////////////
-                ///// Nb: Probably want to store the skeleton/spoke/edge association information to use later!
-                ///////////////////////
+            FloorPlan result = new FloorPlan(footprint);
 
-                //Connect external doors to hallway
-                //Connect vertical features to hallway
-                //  - Either create them on the corridor
-                //  - Or create a new corridor to the vertical
+            
+            //Generate a floor skeleton to lay hallways along and subdivide the floor into regions
+            var regions = GenerateRegions(footprint);
 
-                //Split space into regions (bounded by hallways)
+            //Connect external doors to hallway
+            //Connect vertical features to hallway
+            //  - Either create them on the corridor
+            //  - Or create a new corridor to the vertical
 
-                //Place rooms and shuffle to maximise satisfied constraints (this may be a little complex!)
+            //Split space into regions (bounded by hallways)
 
-                //If a space is passthrough merge it into adjacent hallways and expand it to fill space
-            }
+            //Place rooms and shuffle to maximise satisfied constraints (this may be a little complex!)
 
-            return new FloorPlan(footprint);
+            //If a space is passthrough merge it into adjacent hallways and expand it to fill space
+
+            return result;
         }
 
-        private static void MeasureSkeletonDistance(StraightSkeleton skeleton, out float min, out float max)
+        private static IEnumerable<Edge> GenerateRegions(IReadOnlyList<Vector2> footprint)
         {
-            min = float.MaxValue;
-            max = float.MinValue;
-
-            foreach (var item in skeleton.Skeleton.Select(a => a.Key))
+            using (var straightSkeleton = StraightSkeleton.Generate(footprint))
             {
-                var skeletonVertex = item;
-
-                var edges = new HashSet<LineSegment2>(
-                    from edge in skeleton.Borders
-                    from spoke in skeleton.Spokes
-                    where spoke.Value == skeletonVertex
-                    where edge.Key == spoke.Key || edge.Value == spoke.Key
-                    select new LineSegment2(edge.Key, edge.Value)
-                );
-
-                foreach (var edge in edges)
+                //Slice the floorplan up by the lines of the straight skeleton
+                List<IReadOnlyList<Vector2>> parts = new List<IReadOnlyList<Vector2>> { footprint };
+                foreach (var edge in straightSkeleton.Skeleton)
                 {
-                    var closest = edge.Line.ClosestPoint(skeletonVertex);
-                    var distance = Vector2.Distance(closest, skeletonVertex);
-
-                    min = Math.Min(min, distance);
-                    max = Math.Max(max, distance);
+                    var sliceLine = new Line2D(edge.Start.Position, edge.End.Position - edge.Start.Position);
+                    parts = (from part in parts
+                             from resultPart in part.SlicePolygon(sliceLine)
+                             select (IReadOnlyList<Vector2>)resultPart.ToList()).ToList();
                 }
+
+                DrawOutlines(parts);
+
             }
+
+            return new Edge[0];
         }
 
         #region serialization
         private static Serializer CreateSerializer()
         {
-            var serializer = new Serializer(new SerializerSettings
-            {
+            var serializer = new Serializer(new SerializerSettings {
                 EmitTags = true,
             });
 
@@ -147,13 +142,58 @@ namespace Base_CityGeneration.Elements.Building.Internals.Floors.Design
 
             public FloorDesigner Unwrap()
             {
-                return new FloorDesigner(
-                    Tags,
-                    Guid.Parse(Id ?? Guid.NewGuid().ToString()),
-                    Description,
-                    Rooms.Select(a => a.Unwrap()).ToArray()
-                );
+                return new FloorDesigner(Tags, Guid.Parse(Id ?? Guid.NewGuid().ToString()), Description, Rooms.Select(a => a.Unwrap()).ToArray());
             }
+        }
+        #endregion
+
+        #region Visualisation
+        private static void DrawOutlines(IReadOnlyList<IReadOnlyList<Vector2>> parts)
+        {
+            var svg = new StringBuilder();
+            svg.Append("<svg width=\"1000\" height=\"1000\"><g transform=\"translate(210, 210)\">");
+            foreach (var part in parts)
+                svg.Append(ToSvgPath(part, 10, "blue"));
+            Console.WriteLine(svg);  
+        }
+
+        private static void DrawSkeleton(IEnumerable<Edge> borders, IEnumerable<Edge> spokes, IEnumerable<Edge> skeleton)
+        {
+            var svg = new StringBuilder();
+            svg.Append("<svg width=\"1000\" height=\"1000\"><g transform=\"translate(210, 210)\">");
+            svg.Append(string.Join("", ToSvgPaths(borders, 10, "blue")));
+            svg.Append(string.Join("", ToSvgPaths(spokes, 10, "red")));
+            svg.Append(string.Join("", ToSvgPaths(skeleton, 10, "green")));
+            Console.WriteLine(svg);
+        }
+
+        private static void DrawSkeleton(IEnumerable<Edge> edges)
+        {
+            var svg = new StringBuilder();
+            svg.Append("<svg width=\"1000\" height=\"1000\"><g transform=\"translate(210, 210)\">");
+            svg.Append(string.Join("", ToSvgPaths(edges.Where(e => e.Type == EdgeType.Border), 10, "blue")));
+            svg.Append(string.Join("", ToSvgPaths(edges.Where(e => e.Type == EdgeType.Spoke), 10, "red")));
+            svg.Append(string.Join("", ToSvgPaths(edges.Where(e => e.Type == EdgeType.Skeleton), 10, "green")));
+            Console.WriteLine(svg);
+        }
+
+        private static IEnumerable<string> ToSvgPaths(IEnumerable<Edge> edges, float scale, string color)
+        {
+            foreach (var edge in edges)
+                yield return string.Format("<path fill=\"none\" stroke=\"" + color + "\" d=\"M{0} {1} L{2} {3}\"></path>", edge.Start.Position.X * scale, edge.Start.Position.Y * scale, edge.End.Position.X * scale, edge.End.Position.Y * scale);
+        }
+
+        private static string ToSvgPath(IReadOnlyList<Vector2> shape, float scale, string color)
+        {
+            var builder = new StringBuilder("<path fill=\"none\" stroke=\"" + color + "\" d=\"");
+
+            builder.Append(string.Format("M {0} {1} ", shape[0].X * scale, shape[0].Y * scale));
+            for (var i = 1; i < shape.Count; i++)
+                builder.Append(string.Format("L {0} {1} ", shape[i].X * scale, shape[i].Y * scale));
+
+            builder.Append("Z\"></path>");
+
+            return builder.ToString();
         }
         #endregion
     }
