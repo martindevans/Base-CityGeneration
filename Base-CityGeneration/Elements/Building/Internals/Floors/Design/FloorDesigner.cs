20385f7a35ec5a2828eaaa522ed97a09af5701c7
Martin Evans <martindevans@gmail.com>
2016-03-11 01:30:56 +0000

    Moved growth parameters into a separate class Added vertical elements into growth map

--- Base-CityGeneration/Elements/Building/Internals/Floors/Design/FloorDesigner.cs
+++ Base-CityGeneration/Elements/Building/Internals/Floors/Design/FloorDesigner.cs
@@ -8,7 +8,6 @@ using Base_CityGeneration.Elements.Building.Design;
 using Base_CityGeneration.Elements.Building.Internals.Floors.Design.Planning;
 using Base_CityGeneration.Elements.Building.Internals.Floors.Design.Spaces;
 using Base_CityGeneration.Elements.Building.Internals.Floors.Plan;
-using Base_CityGeneration.Utilities.Extensions;
 using Base_CityGeneration.Utilities.Numbers;
 using EpimetheusPlugins.Scripts;
 using JetBrains.Annotations;
@@ -42,33 +41,23 @@ namespace Base_CityGeneration.Elements.Building.Internals.Floors.Design
 
         private readonly IReadOnlyList<BaseSpaceSpec> _spaces;
 
-        private readonly IValueGenerator _seedSpacing;
-        private readonly IValueGenerator _seedChance;
-        private readonly IValueGenerator _parallelCheckLength;
-        private readonly IValueGenerator _parallelCheckWidth;
-        private readonly IValueGenerator _parallelAngleThreshold;
-        private readonly IValueGenerator _intersectionContinuationChance;
+        private readonly WallGrowthParameters _wallGrowthParameters;
         #endregion
 
         #region constructor
-        private FloorDesigner(Dictionary<string, string> tags, Guid guid, string description, IReadOnlyList<BaseSpaceSpec> spaces, IValueGenerator seedSpacing, IValueGenerator parallelCheckLength, IValueGenerator parallelCheckWidth, IValueGenerator parallelAngleThreshold, IValueGenerator intersectionContinuationChance, IValueGenerator seedChance)
+        private FloorDesigner(Dictionary<string, string> tags, Guid guid, string description, IReadOnlyList<BaseSpaceSpec> spaces, WallGrowthParameters wallGrowthParameters)
         {
             _tags = tags;
             _guid = guid;
             _description = description;
             _spaces = spaces;
 
-            _seedSpacing = seedSpacing;
-            _seedChance = seedChance;
-            _parallelCheckLength = parallelCheckLength;
-            _parallelCheckWidth = parallelCheckWidth;
-            _parallelAngleThreshold = parallelAngleThreshold;
-            _intersectionContinuationChance = intersectionContinuationChance;
+            _wallGrowthParameters = wallGrowthParameters;
         }
         #endregion
 
         #region design
-        public FloorPlanBuilder Design(Func<double> random, INamedDataCollection metadata, Func<KeyValuePair<string, string>[], Type[], ScriptReference> finder, IReadOnlyList<Vector2> footprint, IReadOnlyList<IReadOnlyList<Subsection>> sections, float wallThickness, IList<IReadOnlyList<Vector2>> overlappingVerticals, IReadOnlyList<VerticalSelection> startingVerticals)
+        public FloorPlanBuilder Design(Func<double> random, INamedDataCollection metadata, Func<KeyValuePair<string, string>[], Type[], ScriptReference> finder, IReadOnlyList<Vector2> footprint, IReadOnlyList<IReadOnlyList<Subsection>> sections, float wallThickness, IReadOnlyList<IReadOnlyList<Vector2>> overlappingVerticals, IReadOnlyList<VerticalSelection> startingVerticals)
         {
             Contract.Requires(random != null);
             Contract.Requires(metadata != null);
@@ -81,7 +70,7 @@ namespace Base_CityGeneration.Elements.Building.Internals.Floors.Design
 
             var region = CreateRegion(footprint, sections);
 
-            var planner = new FloorPlanner(random, metadata, finder, wallThickness, _seedSpacing, _parallelCheckLength, _parallelCheckWidth, _parallelAngleThreshold, _intersectionContinuationChance, _seedChance);
+            var planner = new FloorPlanner(random, metadata, finder, wallThickness, _wallGrowthParameters);
             return planner.Plan(region, overlappingVerticals, startingVerticals, _spaces);
         }
 
@@ -154,50 +143,6 @@ namespace Base_CityGeneration.Elements.Building.Internals.Floors.Design
             return CreateSerializer().Deserialize<Container>(reader).Unwrap();
         }
 
-        /// <summary>
-        /// Parameters passed into the wall growth generator
-        /// </summary>
-        internal class GrowthParameters
-        {
-            /// <summary>
-            /// Parameters for parallelism checking
-            /// </summary>
-            public ParallelCheckParameters? ParallelCheck { get; [UsedImplicitly] set; }
-
-            /// <summary>
-            /// Distance between seeds along walls
-            /// </summary>
-            public object SeedSpacing { get; [UsedImplicitly] set; }
-
-            /// <summary>
-            /// Chance that a seed will be placed at a seed point (otherwise the wall will continue straight on)
-            /// </summary>
-            public object SeedChance { get; [UsedImplicitly] set; }
-
-            /// <summary>
-            /// When a wall hits another wall chance that the wall will continue out the other side
-            /// </summary>
-            public object IntersectionContinuationChance { get; [UsedImplicitly] set; }
-        }
-
-        internal struct ParallelCheckParameters
-        {
-            /// <summary>
-            /// Multiplier of length to check for parallel walls (e.g. 1.5 to check for the length of the proposed wall and half again)
-            /// </summary>
-            public object Length { get; set; }
-
-            /// <summary>
-            /// Distance to check for parallel walls either side
-            /// </summary>
-            public object Width { get; set; }
-
-            /// <summary>
-            /// Maximum angle between this wall and the other to consider parallel
-            /// </summary>
-            public object Angle { get; set; }
-        }
-
         internal class Container
             : IUnwrappable<FloorDesigner>
         {
@@ -208,7 +153,7 @@ namespace Base_CityGeneration.Elements.Building.Internals.Floors.Design
             public string Id { get; [UsedImplicitly] set; }
             public string Description { get; [UsedImplicitly] set; }
 
-            public GrowthParameters GrowthParameters { get; [UsedImplicitly] set; }
+            public WallGrowthParameters.Container GrowthParameters { get; [UsedImplicitly] set; }
 
             public List<BaseSpaceSpec.BaseContainer> Spaces { get; [UsedImplicitly] set; }
             // ReSharper restore CollectionNeverUpdated.Global
@@ -216,28 +161,12 @@ namespace Base_CityGeneration.Elements.Building.Internals.Floors.Design
 
             public FloorDesigner Unwrap()
             {
-                Contract.Assert(GrowthParameters != null
-                             && GrowthParameters.SeedSpacing != null
-                );
-
-                var defaultParallel = new ParallelCheckParameters {
-                    Length = 1.25f,
-                    Width = 1,
-                    Angle = 10
-                };
-                var parallelParams = GrowthParameters.ParallelCheck ?? defaultParallel;
-
                 return new FloorDesigner(
                     Tags,
                     Guid.Parse(Id ?? Guid.NewGuid().ToString()),
                     Description ?? "",
                     Spaces.Select(a => a.Unwrap()).ToArray(),
-                    IValueGeneratorContainer.FromObject(GrowthParameters.SeedSpacing, new NormallyDistributedValue(2.5f, 5, 6.5f, 1.5f)),
-                    IValueGeneratorContainer.FromObject(parallelParams.Length, defaultParallel.Length),
-                    IValueGeneratorContainer.FromObject(parallelParams.Width, defaultParallel.Width),
-                    IValueGeneratorContainer.FromObject(parallelParams.Angle, defaultParallel.Angle).Transform(Microsoft.Xna.Framework.MathHelper.ToRadians),
-                    IValueGeneratorContainer.FromObject(GrowthParameters.IntersectionContinuationChance, 0.75f),
-                    IValueGeneratorContainer.FromObject(GrowthParameters.SeedChance, 0.5f)
+                    GrowthParameters.Unwrap()
                 );
             }
         }
--- Base-CityGeneration/Elements/Building/Internals/Floors/Design/Planning/FloorPlanner.cs
+++ Base-CityGeneration/Elements/Building/Internals/Floors/Design/Planning/FloorPlanner.cs
@@ -3,50 +3,45 @@ using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Numerics;
+using Base_CityGeneration.Datastructures.Extensions;
 using Base_CityGeneration.Elements.Building.Design;
 using Base_CityGeneration.Elements.Building.Internals.Floors.Design.Spaces;
 using Base_CityGeneration.Elements.Building.Internals.Floors.Plan;
 using Base_CityGeneration.Utilities.Numbers;
+using EpimetheusPlugins.Extensions;
 using EpimetheusPlugins.Scripts;
 using Myre.Collections;
+using PrimitiveSvgBuilder;
 
 namespace Base_CityGeneration.Elements.Building.Internals.Floors.Design.Planning
 {
+    /// <summary>
+    /// Internal implementation of the logic used in FloorDesigner.
+    /// FloorDesigner is mostly concerned with gathering up the data needed and formatting it to pass into here for actual work to happen
+    /// </summary>
     internal class FloorPlanner
     {
         private readonly Func<double> _random;
         private readonly INamedDataCollection _metadata;
         private readonly Func<KeyValuePair<string, string>[], Type[], ScriptReference> _finder;
         private readonly float _wallThickness;
+        private readonly WallGrowthParameters _wallGrowthParameters;
 
-        private readonly IValueGenerator _seedSpacing;
-        private readonly IValueGenerator _seedChance;
-        private readonly IValueGenerator _parallelCheckLength;
-        private readonly IValueGenerator _parallelCheckWidth;
-        private readonly IValueGenerator _parallelAngleThreshold;
-        private readonly IValueGenerator _intersectionContinuationChance;
-
-        public FloorPlanner(Func<double> random, INamedDataCollection metadata, Func<KeyValuePair<string, string>[], Type[], ScriptReference> finder, float wallThickness, IValueGenerator seedSpacing, IValueGenerator parallelCheckLength, IValueGenerator parallelCheckWidth, IValueGenerator parallelAngleThreshold, IValueGenerator intersectionContinuationChance, IValueGenerator seedChance)
+        public FloorPlanner(Func<double> random, INamedDataCollection metadata, Func<KeyValuePair<string, string>[], Type[], ScriptReference> finder, float wallThickness, WallGrowthParameters wallGrowthParameters)
         {
             Contract.Requires(random != null);
             Contract.Requires(metadata != null);
             Contract.Requires(finder != null);
-            Contract.Requires(seedSpacing != null);
+            Contract.Requires(wallGrowthParameters != null);
 
             _random = random;
             _metadata = metadata;
             _finder = finder;
             _wallThickness = wallThickness;
-
-            _seedSpacing = seedSpacing;
-            _seedChance = seedChance;
-            _parallelCheckLength = parallelCheckLength;
-            _parallelCheckWidth = parallelCheckWidth;
-            _parallelAngleThreshold = parallelAngleThreshold;
-            _intersectionContinuationChance = intersectionContinuationChance;
+            _wallGrowthParameters = wallGrowthParameters;
         }
 
-        public FloorPlanBuilder Plan(Region region, IList<IReadOnlyList<Vector2>> overlappingVerticals, IReadOnlyList<VerticalSelection> startingVerticals, IReadOnlyList<BaseSpaceSpec> spaces)
+        public FloorPlanBuilder Plan(Region region, IReadOnlyList<IReadOnlyList<Vector2>> overlappingVerticals, IReadOnlyList<VerticalSelection> startingVerticals, IReadOnlyList<BaseSpaceSpec> spaces)
         {
             var builder = new FloorPlanBuilder(region.Points.ToArray());
 
@@ -56,17 +51,46 @@ namespace Base_CityGeneration.Elements.Building.Internals.Floors.Design.Planning
             throw new NotImplementedException();
         }
 
-        private void PlanRegion(FloorPlanBuilder builder, Region region, IList<IReadOnlyList<Vector2>> overlappingVerticals, IReadOnlyList<VerticalSelection> startingVerticals, IReadOnlyList<BaseSpaceSpec> spaces)
+        private void PlanRegion(FloorPlanBuilder builder, Region region, IReadOnlyList<IReadOnlyList<Vector2>> overlappingVerticals, IReadOnlyList<VerticalSelection> startingVerticals, IReadOnlyList<BaseSpaceSpec> spaces)
         {
-            var map = new GrowthMap(region.Points.ToArray(), _seedSpacing, _random, _metadata, _parallelCheckLength, _parallelCheckWidth, _parallelAngleThreshold, _intersectionContinuationChance, _seedChance);
-            map.Grow();
+            //Grow floorplan for region
+            var map = new GrowthMap(region.Points.ToArray(), overlappingVerticals, _random, _metadata, _wallGrowthParameters).Grow();
 
-            //todo: seed along edges of region
-            //todo: grow out from seeds (perpendicular to edge, or equally splitting an internal angle > 180)
-            //todo: intersections form rooms, remove rooms which are too small (by merging into neighbouring rooms)
+            //Remove rooms which are too small
+            var undersized = map.Faces.Where(a => a.Area() < 10);
+            //todo: look through specs we want to fit into this plan and find the smallest, that's our min area
+            // ^ consider just dolling out specs, and then merging spaces which have nothing assigned
+
+            //todo: remove temp visualisation code
+            var svg = new SvgBuilder(10);
+            foreach (var face in map.Faces)
+            {
+                var col = VertexVariance(face.Vertices.Select(a => a.Position).ToArray()) > 1.75f ? "red" : "cornflowerblue";
+                svg.Outline(face.Vertices.Select(a => a.Position).ToArray(), stroke: "none", fill: col);
+            }
+            foreach (var edge in map.HalfEdges.Where(a => a.IsPrimaryEdge))
+                svg.Line(edge.StartVertex.Position, edge.EndVertex.Position, 1, "black");
+            foreach (var vertex in map.Vertices)
+                svg.Circle(vertex.Position, 0.2f, "black");
+            Console.WriteLine(svg.ToString());
+
             //todo: order specs by constraints (most difficult to solve first), assign specs to spaces generated (best fit)
             //todo: connectivity (doors + corridors)
             //todo: recursive for groups
+        }
+
+        private float VertexVariance(IReadOnlyList<Vector2> vertices)
+        {
+            var min = float.PositiveInfinity;
+            var max = float.NegativeInfinity;
+
+            foreach (var avgDist in vertices.Select(v => vertices.Select(a => Vector2.Distance(a, v)).Sum() / vertices.Count))
+            {
+                min = Math.Min(avgDist, min);
+                max = Math.Max(avgDist, max);
+            }
+
+            return max / min;
         }
     }
 }
