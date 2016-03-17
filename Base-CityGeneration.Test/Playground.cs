c09e4375b8d403b62e7f250245dbc9570445cee0
Martin <Martin@martin-pc>
2016-03-17 19:00:37 +0000

    Implemented final room merging algorithm (tuned by 6 parameters - threshold and weight for area, convexity and angular deviation)

--- Base-CityGeneration.Test/Playground.cs
+++ Base-CityGeneration.Test/Playground.cs
@@ -11,14 +11,14 @@ namespace Base_CityGeneration.Test
         [TestMethod]
         public void TestMethod1()
         {
-            Vector2 a = Vector2.Normalize(new Vector2(0, 1));
-            Vector2 b = Vector2.Normalize(new Vector2(-1, 1));
+            var a = Vector2.Normalize(new Vector2(0, 1));
+            var b = Vector2.Normalize(new Vector2(0, 1));
 
             var dot = Vector2.Dot(a, b);
             var det = a.Cross(b);
             var angle = -(float)Math.Atan2(det, dot);
 
-            Console.WriteLine(angle);
+            Console.WriteLine(dot);
         }
     }
 }
--- Base-CityGeneration/Elements/Building/Internals/Floors/Design/Planning/FloorplanHalfEdgeTags.cs
+++ Base-CityGeneration/Elements/Building/Internals/Floors/Design/Planning/FloorplanHalfEdgeTags.cs
@@ -6,6 +6,7 @@ using Base_CityGeneration.Datastructures.HalfEdge;
 using EpimetheusPlugins.Extensions;
 using System.Numerics;
 using Myre.Extensions;
+using Placeholder.ConstructiveSolidGeometry;
 using SwizzleMyVectors.Geometry;
 
 namespace Base_CityGeneration.Elements.Building.Internals.Floors.Design.Planning
@@ -28,6 +29,7 @@ namespace Base_CityGeneration.Elements.Building.Internals.Floors.Design.Planning
         : BaseFaceTag<FloorplanVertexTag, FloorplanHalfEdgeTag, FloorplanFaceTag>
     {
         public float AngularDeviation { get; private set; }
+        public float Convexity { get; private set; }
 
         public bool Mergeable { get; private set; }
 
@@ -42,38 +44,39 @@ namespace Base_CityGeneration.Elements.Building.Internals.Floors.Design.Planning
         {
             base.Attach(f);
 
-            AngularDeviation = CalculateAngularVariance(f.Edges);
+            AngularDeviation = CalculateAngularDeviation(f.Edges);
+            Convexity = CalculateConvexity(f.Vertices.Select(v => v.Position));
         }
 
-        #region angular variance
-        private static float CalculateAngularVariance(IEnumerable<HalfEdge<FloorplanVertexTag, FloorplanHalfEdgeTag, FloorplanFaceTag>> edges)
+        #region convexity
+        private static float CalculateConvexity(IEnumerable<Vector2> vertices)
         {
-            ////var smallAngles = edges
-            ////    .Select(ab => Vector2.Dot(ab.Segment.Line.Direction, -ab.Next.Segment.Line.Direction))
-            ////    .Where(dot => dot < 0)
-            ////    .Select(dot => -dot)
-            ////    .Append(0f)
-            ////    .Sum();
+            var area = vertices.Area();
+            var convexHullArea = vertices.ConvexHull().Area();
 
-            ////if (Math.Abs(smallAngles) < float.Epsilon)
-            ////    return 0;
-
-            ////return smallAngles / edges.Count();
-
-            var dots = edges.Select(ab => Vector2.Dot(ab.Segment.Line.Direction, -ab.Next.Segment.Line.Direction)).ToArray();
-            return CalculateAngularVariance(dots);
-
-            ////calculate dot product between lines
-            //return (float)Math.Sqrt(edges
-            //    .Select(ab => Vector2.Dot(ab.Segment.Line.Direction, -ab.Next.Segment.Line.Direction)) //Measure angle between lines
-            //    .Where(dot => !dot.TolerantEquals(-1, 0.015192f)) //Exclude angles which are nearly 180 degrees (to within 10 degrees)
-            //    .Select(a => (float)Math.Acos(a))
-            //    .Variance()
-            //);
+            return area / convexHullArea ;
         }
 
-        public static float CalculateAngularVariance(IEnumerable<Vertex<FloorplanVertexTag, FloorplanHalfEdgeTag, FloorplanFaceTag>> vertices)
+        public static float CalculateConvexity(IEnumerable<Vertex<FloorplanVertexTag, FloorplanHalfEdgeTag, FloorplanFaceTag>> vertices)
         {
+            return CalculateConvexity(vertices.Select(v => v.Position));
+        }
+        #endregion
+
+        #region angular deviation
+        private static float CalculateAngularDeviation(IEnumerable<HalfEdge<FloorplanVertexTag, FloorplanHalfEdgeTag, FloorplanFaceTag>> edges)
+        {
+            Contract.Requires(edges != null);
+
+            return CalculateAngularDeviation(edges
+                .Select(ab => Vector2.Dot(ab.Segment.Line.Direction, ab.Next.Segment.Line.Direction))
+            );
+        }
+
+        public static float CalculateAngularDeviation(IEnumerable<Vertex<FloorplanVertexTag, FloorplanHalfEdgeTag, FloorplanFaceTag>> vertices)
+        {
+            Contract.Requires(vertices != null);
+
             //Zip with the next vertex around and calculate line segments
             var edges = vertices.Zip(vertices.Skip(1).Concat(vertices.Take(1)), (a, b) => new LineSegment2(a.Position, b.Position)).ToArray();
 
@@ -81,29 +84,20 @@ namespace Base_CityGeneration.Elements.Building.Internals.Floors.Design.Planning
             var dots = edges.ZipWithIndex().Select(abi => {
                 var ab = abi.Value;
                 var bc = edges[(abi.Key + 1) % edges.Length];
-                return Vector2.Dot(ab.Line.Direction, -bc.Line.Direction);
+                return Vector2.Dot(ab.Line.Direction, bc.Line.Direction);
             });
 
-            return CalculateAngularVariance(dots);
+            return CalculateAngularDeviation(dots);
         }
 
-        private static float CalculateAngularVariance(IEnumerable<float> dots)
+        private static float CalculateAngularDeviation(IEnumerable<float> dots)
         {
-            var small = 0f;
-            var ok = 1;
-            foreach (var dot in dots)
-            {
-                if (dot >= 0)
-                {
-                    //Ignore straight lines (dot == 1 indicates a straight line with a vertex in the middle)
-                    if (dot < 0.99f)
-                        ok++;
-                }
-                else
-                    small += (float)Math.Pow(-dot, 0.1f);
-            }
-
-            return small / ok;
+            return (float)Math.Sqrt(
+                dots
+                    .Where(dot => !dot.TolerantEquals(1, 0.015192f)) //Exclude angles which are nearly 0 degrees (to within 10 degrees)
+                    .Select(a => (float)Math.Acos(a))
+                    .Variance()
+            );
         }
         #endregion
 
